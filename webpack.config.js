11b1cefc82b5d40ee1664a179dba7aea57230a43
hi-ueda <hi-ueda@m3career.com>
2024-01-30 18:59:16 +0900

    jsのimportの実験

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,14 @@
+const path = require('path')
+
 module.exports = {
-  context: __dirname + "/src",
-  entry: './src/index.js',
+  context: path.join(__dirname, "/src"),
+  // webpackががビルドを始めるとき、どのファイルを起点にビルドするかを指定する。
+  entry: './index.js',
+  // ビルドしたファイルをどこにどんな名前で出力するかを指定する。 
+  output: {
+    path: `${__dirname}/dist`,
+    filename: "./assets/js/main.js",
+  },
+  devtool: "hidden-source-map",
+  mode: "development",
 }
